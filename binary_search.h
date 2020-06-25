97a8d416830e349a7cbc30d8af886f013eb91c89
allan-cedric <allan.py3000@gmail.com>
2020-06-25 20:35:39 -0300

    header file pronto binary_search.h

---
+++ binary_search.h
@@ -0,0 +1,30 @@
+/*
+    Header file: binary_search.h
+*/
+
+/*
+    Protótipo da função de busca binária
+    em um vetor de inteiros
+*/
+
+/*
+    Autor: Allan Cedric
+*/
+
+#ifndef __BINARY_SEARCH_H__
+#define __BINARY_SEARCH_H__
+
+/*
+    Bibliotecas padrões
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+    Busca o elemento(value) no vetor(v),
+    se encontrar retorna o índice que se encontra o elemento,
+    senão -1
+*/
+int binarySearch(int *v, int value);
+
+#endif
